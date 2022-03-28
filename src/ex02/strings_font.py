d7a0bb0a0ce2af2d17e14073a96b15beb993a6f6
André Jesus <74548852+Andre-J3sus@users.noreply.github.com>
2022-03-28 18:07:38 +0100

    Implemented by me and Nyck: exercise02a implemented.

---
+++ src/ex02/strings_font.py
@@ -0,0 +1,64 @@
+import random
+import math
+import matplotlib.pyplot as plt
+
+
+def strings_font(alphabet, fmp, n, write_file, show_hist):
+    """
+    Generates a sequence of L strings, separated by ";".
+    Prints the entropy.
+
+    :param alphabet: list of possible strings
+    :param fmp: function of mass probability for the alphabet
+    :param n: number of strings to generate
+    :param write_file when True, writes the sequence in a file
+    :param show_hist when True, shows the sequence histogram
+
+    :return: sequence of strings
+
+    Example: strings_font(["abc", "cde"], [0.7, 0.3], 3) -> "abc;abc;cde"
+    """
+
+    # Calculate and print entropy
+    entropy = calculate_entropy(fmp)
+    print("Entropy: ", entropy)
+
+    # Get string sequence
+    strs = random.choices(alphabet, fmp, k=n)
+    res = ";".join(strs)
+    print("Sequence: ", res)
+
+    # Write sequence in file
+    if write_file:
+        with open("strings_font", "w") as f:
+            f.write(res)
+
+    # Display sequence histogram
+    if show_hist:
+        plt.hist(strs, bins=len(strs))
+        plt.xlabel("Alphabet")
+        plt.ylabel("Occurrences")
+        plt.suptitle("Strings Font Histogram", fontsize=18)
+        plt.title("Entropy: " + str(calculate_entropy(fmp)))
+        plt.show()
+
+    return res
+
+
+def calculate_entropy(fmp):
+    """
+    Calculates the entropy of a function of mass probability.
+
+    :param fmp: function of mass probability
+
+    :return: entropy
+    """
+
+    return sum(map(lambda prob: prob * math.log(prob, 2), fmp)) * -1
+
+
+#  Test of strings_font
+a = ["abc", "hbo", "xpto", "brba", "cbo", "foo", "bar"]
+a_fmp = [3 / 16, 1 / 16, 6 / 16, 2 / 16, 1 / 16, 2 / 16, 1 / 16]
+
+strings_font(a, a_fmp, 100, write_file=True, show_hist=True)
